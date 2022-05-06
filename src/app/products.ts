81a59566ec2942f295becb437577c8707b2e047d
YoungBrisy <londonbigban@gmail.com>
2022-05-06 22:29:32 +1000

    fist commit for Angular original homepage example

---
+++ src/app/products.ts
@@ -0,0 +1,45 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+  },
+  {
+    id: 4,
+    name: 'Galaxy Note 10 plus',
+    price: 1100,
+    description: "Young's best phone",
+  },
+  {
+    id: 5,
+    name: 'Iphone XS',
+    price: 1100,
+    description: "Shin's best phone",
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
